2362c51b6110de8cc117243787e546648f9e91a9
Sajid Moinuddin <sajid.moinuddin@kayosports.com.au>
2020-05-19 13:08:34 +1000

    fix log level

--- src/main/python/watchman/local_computer_newrelic_event_handler.py
+++ src/main/python/watchman/local_computer_newrelic_event_handler.py
@@ -13,11 +13,13 @@ class LocalComputerEventHandler:
         Utils.whistle(2)
         Utils.append_to_logfile(event.print_full())
         text_2_speech = f"{event.cluster} {event.event_type}! {event.reason} {event.kind}!, {event.object_name}!, namespace: {event.namespace}" 
+        
         Utils.text_2_speech(text_2_speech)
+        pi_display_txt = f"{event.cluster} \nnamespace: {event.namespace}\n{event.event_type}! \n{event.reason} {event.kind}!, \n{event.object_name}!"
         # Utils.text_2_speech(f"summary: {event.message}")
         logging.info("____>>>> %s", event.print_full())
-        logging.info("____>>>> \n\n%s", text_2_speech)
-        logging.debug("---------------------------------------------------------------------------------------")
+        logging.info("____>>>> \n\n%s", pi_display_txt)
+        logging.debug("------------------")
         
     def startListen(self):
         pub.subscribe(self.on_new_event, 'newrelic.kubernetes')
