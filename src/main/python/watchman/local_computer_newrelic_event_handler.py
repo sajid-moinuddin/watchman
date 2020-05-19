b6dc4ae5caf468f42540a0accb31633e796a9c02
Sajid Moinuddin <sajid.moinuddin@kayosports.com.au>
2020-05-19 13:30:54 +1000

    x

--- src/main/python/watchman/local_computer_newrelic_event_handler.py
+++ src/main/python/watchman/local_computer_newrelic_event_handler.py
@@ -12,13 +12,14 @@ class LocalComputerEventHandler:
     def on_new_event(self, event: K8sEvent):
         Utils.whistle(2)
         Utils.append_to_logfile(event.print_full())
-        text_2_speech = f"{event.cluster} {event.event_type}! {event.reason} {event.kind}!, {event.object_name}!, namespace: {event.namespace}" 
-        
-        Utils.text_2_speech(text_2_speech)
+
         pi_display_txt = f"{event.cluster} \nnamespace: {event.namespace}\n{event.event_type}! \n{event.reason} {event.kind}!, \n{event.object_name}! \nsummary: {event.message}"
-        # Utils.text_2_speech(f"summary: {event.message}")
         logging.info("____>>>> %s", event.print_full())
         logging.info("____>>>> \n\n%s", pi_display_txt)
+
+        text_2_speech = f"{event.cluster} {event.event_type}! {event.reason} {event.kind}!, {event.object_name}!, namespace: {event.namespace}"         
+        Utils.text_2_speech(text_2_speech)
+        # Utils.text_2_speech(f"summary: {event.message}")
         logging.debug("------------------")
         
     def startListen(self):
